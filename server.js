75038ed00fa85c011b832872a66d30b84133fc9e
Jacob Markovich <markovich.jacob@gmail.com>
2020-07-12 21:34:33 -0500

    formatting

--- server.js
+++ server.js
@@ -2,32 +2,34 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 // Start up an instance of app
 const app = express();
 /* Middleware*/
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 // Setup Server
 const port = 3030;
-const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
-
-app.get('/all', (req, res) => {
-  res.send(JSON.stringify(projectData))
+const server = app.listen(port, () => {
+    console.log(`running on localhost: ${port}`);
 });
 
-app.post('/', (req, res) => {
-  projectData.date = req.body.date
-  projectData.temperature = req.body.temperature
-  projectData.feelings = req.body.feelings
-  res.end()
+app.get("/all", (req, res) => {
+    res.send(JSON.stringify(projectData));
+});
+
+app.post("/", (req, res) => {
+    projectData.date = req.body.date;
+    projectData.temperature = req.body.temperature;
+    projectData.feelings = req.body.feelings;
+    res.end();
 });
--- website/app.js
+++ website/app.js
@@ -1,56 +1,58 @@
 /* Global Variables */
-const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = '&appid=9f3cc97527bc254a864395a08dde41f8';
-const newZip = document.getElementById('zip');
-const yourFeels = document.getElementById('feelings');
-const postURL = 'http://localhost:3030';
-const getURL = 'http://localhost:3030/all';
-const dateHolder = document.getElementById('date')
-const tempHolder = document.getElementById('temp')
-const contentHolder = document.getElementById('content')
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&appid=9f3cc97527bc254a864395a08dde41f8";
+const newZip = document.getElementById("zip");
+const yourFeels = document.getElementById("feelings");
+const postURL = "http://localhost:3030";
+const getURL = "http://localhost:3030/all";
+const dateHolder = document.getElementById("date");
+const tempHolder = document.getElementById("temp");
+const contentHolder = document.getElementById("content");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
-const generate = async ()=> {
-  getTemp(baseURL, newZip.value, apiKey)
-}
-
-const getTemp = async (link, zip, key)=>{
-  const res = await fetch(`${link}${zip}${key}`)
-  try {
-    const apiData = await res.json();
-    console.log(apiData.main.temp);
-    const data = {
-      date: newDate,
-      temperature: apiData.main.temp,
-      feelings: yourFeels.value
+const generate = async () => {
+    getTemp(baseURL, newZip.value, apiKey);
+};
+//creates the link to fetch the API data then runs the post function and updateUI functions
+const getTemp = async (link, zip, key) => {
+    const res = await fetch(`${link}${zip}${key}`);
+    try {
+        const apiData = await res.json();
+        console.log(apiData.main.temp);
+        const data = {
+            date: newDate,
+            temperature: apiData.main.temp,
+            feelings: yourFeels.value,
+        };
+        await postData(postURL, data);
+        updateUI();
+        return data;
+    } catch (error) {
+        //displays a warning if the zip code entered is invalid
+        alert("Invalid Zip Code!");
+        console.log("error", error);
     }
-    await postData(postURL, data)
-    updateUI()
-    return data;
-  }
-  catch(error) {
-    alert('Invalid Zip Code!')
-    console.log('error', error);
-  }
-}
-const postData = async (path, data = {})=>{
-  const response = await fetch(path, {
-    method: 'POST',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-}
+};
+//posts the data to the server
+const postData = async (path, data = {}) => {
+    const response = await fetch(path, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+};
+// updates the UI with the date, user's feelings, and temp from API
 const updateUI = async () => {
-  const response = await fetch(getURL)
-  const jsonResponse = await response.json()
-  dateHolder.innerHTML = `<span class="entry-item">Date: </span>${jsonResponse.date}`
-  contentHolder.innerHTML = `<span class="entry-item">You feel: </span>${jsonResponse.feelings}`
-  tempHolder.innerHTML = `<span class="entry-item">Temperature: </span>${jsonResponse.temperature}`
-}
-document.getElementById('generate').addEventListener('click', generate);
+    const response = await fetch(getURL);
+    const jsonResponse = await response.json();
+    dateHolder.innerHTML = `<span class="entry-item">Date: </span>${jsonResponse.date}`;
+    contentHolder.innerHTML = `<span class="entry-item">You feel: </span>${jsonResponse.feelings}`;
+    tempHolder.innerHTML = `<span class="entry-item">Temperature: </span>${jsonResponse.temperature}`;
+};
+document.getElementById("generate").addEventListener("click", generate);
